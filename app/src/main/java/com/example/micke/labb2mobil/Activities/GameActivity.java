5b8d404c1532ffbe0f3e7deb8f025b7e2dc38547
dpham <dpham@kth.se>
2017-11-28 12:55:29 +0100

    game activity

--- app/src/main/java/com/example/micke/labb2mobil/Activities/GameActivity.java
+++ app/src/main/java/com/example/micke/labb2mobil/Activities/GameActivity.java
@@ -3,14 +3,18 @@ package com.example.micke.labb2mobil.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.micke.labb2mobil.Activities.View.GameView;
 import com.example.micke.labb2mobil.R;
 
 
 public class GameActivity extends AppCompatActivity {
 
+    private GameView gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        gameView= (GameView) findViewById(R.id.gameView);
     }
 }
--- app/src/main/java/com/example/micke/labb2mobil/Activities/LoadGameActivity.java
+++ app/src/main/java/com/example/micke/labb2mobil/Activities/LoadGameActivity.java
@@ -10,13 +10,7 @@ import com.example.micke.labb2mobil.Activities.View.GameView;
 import com.example.micke.labb2mobil.R;
 
 public class LoadGameActivity extends AppCompatActivity {
-    private GameView gameView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_game);
 
-        gameView= (GameView) findViewById(R.id.gameView);
 
-    }
+
 }
