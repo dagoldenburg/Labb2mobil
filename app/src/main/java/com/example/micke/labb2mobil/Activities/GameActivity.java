9713920cd07d065ed9829352d84fe312f77f9e20
dagoldenburg <dagoldenburg@gmail.com>
2017-11-30 17:34:36 +0100

    event

--- app/src/main/java/com/example/micke/labb2mobil/Activities/GameActivity.java
+++ app/src/main/java/com/example/micke/labb2mobil/Activities/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.micke.labb2mobil.Activities;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,10 +18,29 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        setContentView(R.layout.activity_game);
+        ConstraintLayout v = (ConstraintLayout) findViewById(R.id.activity_game);
+
         ViewState.getViewState().onTilt(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.game_menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ng:
+                        Log.i("asd", "NEW GAME");
+                        return true;
+                    case R.id.sg:
+                        Log.i("asd", "SAVE GAME");
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        v.addView(new GameView(this));
+        toolbar.bringToFront();
     }
-
-
-
 }
